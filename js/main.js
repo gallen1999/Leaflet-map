58435ea0d680f910aad6ed57a187ca32d22b5310
gallen1999 <gallen1999@live.com>
2020-04-05 22:14:40 -0400

    mobile

---
+++ js/main.js
@@ -0,0 +1,45 @@
+var map = L.map('map').setView([43.1594, -79.2469], 12);
+
+	L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+		maxZoom: 15,
+		attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+			'<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+			'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+		id: 'mapbox/streets-v11',
+		tileSize: 512,
+		zoomOffset: -1
+    }).addTo(map);
+
+    map.locate({setView: true, maxZoom: 16});
+    function onLocationFound(e) {
+        var radius = e.accuracy;
+    
+        L.marker(e.latlng).addTo(map)
+            .bindPopup("You are within " + radius + " meters from this point").openPopup();
+    
+        L.circle(e.latlng, radius).addTo(map);
+    }
+    
+    map.on('locationfound', onLocationFound);
+
+    function onLocationError(e) {
+        alert(e.message);
+    }
+    
+    map.on('locationerror', onLocationError);
+    
+
+    var marker = L.marker([43.1594, -79.2469]).addTo(map);
+    
+
+    var m2 = L.marker([43.1594, -79.3269]);
+    var popup = L.popup();
+   function onMapClick(e) {
+        var latlngstring = e.latlng.toString().replace("LatLng", "");
+        m2.setLatLng(e.latlng).addTo(map);
+        m2.bindPopup(latlngstring).openPopup();
+    }
+    map.on('click', onMapClick);
+
+
+    marker.bindPopup("<b>St Catharines!</b><br>where to next?");
